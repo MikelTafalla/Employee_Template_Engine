c4e97f68a2cc4ede23ddd2266645dc27017bd7bd
Mikel1991 <mikel362d@gmail.com>
2020-06-02 19:11:30 -0700

    Manager test created

--- app.js
+++ app.js
@@ -15,64 +15,77 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 const questions = [
-    { 
-      type: "input",
-      message: "What is your teammate's name?",
-      name: "name"
-    },
-    { 
-      type: "input",
-      message: "What is your teammate's ID?",
-      name: "id"
-    },
-    { 
-      type: "input",
-      message: "What is your teammate's email?",
-      name: "email"
-    },
-    { 
-      type: "list",
-      message: "What is your teammate's role?",
-      choices: ["Manager", "Engineer", "Intern"],
-      name: "role"
-    },
-    //if manager ask office number if engineer ask github user name if intern ask school
-    { 
-      type: "list",
-      message: "Would you like to add another team member?",
-      choices: ["yes", "no"],
-      name: "restart"
-    }
+  { 
+    type: "input",
+    message: "What is your teammate's name?",
+    name: "name"
+  },
+  { 
+    type: "input",
+    message: "What is your teammate's ID?",
+    name: "id"
+  },
+  { 
+    type: "input",
+    message: "What is your teammate's email?",
+    name: "email"
+  },
+  { 
+    type: "list",
+    message: "What is your teammate's role?",
+    choices: ["Manager", "Engineer", "Intern"],
+    name: "role"
+  }
 ];
+//if manager ask office number if engineer ask github user name if intern ask school
 const managerQuestions = {
   type: "input",
   message: "What is your manager's office number?",
   name: "officenumber"
 };
+const engineerQuestions = {
+  type: "input",
+  message: "What is your engineer's github user name?",
+  name: "github"
+};
+const internQuestions = {
+  type: "input",
+  message: "What is your intern's school?",
+  name: "school"
+};
+//Add more employees
+const restart = {
+  type: "list",
+  message: "Would you like to add another team member?",
+  choices: ["yes", "no"],
+  name: "restart"
+};
 
 //Array to hold the answers
-const answersArr = [];
+const employeeArr = [];
 //Function to start inquirer
 async function init() {
 
   const userResponse = await inquirer.prompt(questions);
   //Deconstruct reponses
-  const {name, id, email, role, restart} = userResponse;
-  console.log(userResponse);
+  const {name, id, email, role} = userResponse;
+
   // If role is manager, ask for office number. With that response back, then create a new manager with userResponse and officeNumber response like: const employee = new Manager(name, id, email, officeNumber);
   if (role === "Manager") {
-    const reponseoffice = await inquirer.prompt(managerQuestions);
-    console.log(name);
+    const officeNumber = await inquirer.prompt(managerQuestions);
+
+    const employee = new Manager(name, id, email, officeNumber);
+
+    console.log(employee);
     // push the manager constructor object into employeesArr
+    employeeArr.push(employee);
   }
     //If user wants to add another team member, we send the answer to the answerArr array and ask the questions again. If finished, we send the answer to the array and stop the function.
     // Ask them if they want to add another employee
   if (restart === "yes") {
-      answersArr.push(userResponse);
       init();
   } else {
-      answersArr.push(userResponse);
-      return console.log(answersArr);
+      return;
   };
 
 };
@@ -81,7 +94,7 @@ init();
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-render(answersArr);
+
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
